4ea05fe42772735f141ff5ae66d30d7b27972ac5
Syrax-Prog <affan2088a@gmail.com>
2026-06-23 15:51:54 +0800

    jbljkb

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,6 @@ use App\Http\Controllers\Movie;
 use App\Http\Controllers\Index;
 
 Route::any('/get_movie/{any?}/{any2?}', [Movie::class, 'get_movie']);
+Route::any('/', [Movie::class, 'get_movie']);
 Route::any('/test', [Movie::class, 'test']);
 Route::any('/login', [Index::class, 'index']);
